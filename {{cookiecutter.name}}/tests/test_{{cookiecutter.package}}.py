3159d78ee2bfcbae8068db9b414b0e296817f490
Jon Robison <narfman0@gmail.com>
2019-03-21 15:20:59 +0000

    -Couple minor fixes for successful test running

--- {{cookiecutter.name}}/tests/test_{{cookiecutter.package}}.py
+++ {{cookiecutter.name}}/tests/test_{{cookiecutter.package}}.py
@@ -4,4 +4,4 @@ from {{ cookiecutter.package }}.{{ cookiecutter.package }} import logic
 
 class Test{{ cookiecutter.package }}(unittest.TestCase):
     def test_{{ cookiecutter.package }}(self):
-        self.assertEqual('Hello World!', logic())
+        self.assertEqual("Hello World!", logic())
